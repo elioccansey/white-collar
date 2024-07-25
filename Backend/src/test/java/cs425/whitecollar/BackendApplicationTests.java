58973304163d6591961ed418eaf260b548d88e97
Alembrhan23 <alembrhan23@gmail.com>
2024-07-25 15:41:32 -0600

    initial commit

---
+++ Backend/src/test/java/cs425/whitecollar/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cs425.whitecollar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
